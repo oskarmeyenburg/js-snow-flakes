cb11e87a13b7baa381b2f3626224d4b758d863a2
oskarmeyenburg <oskar.meyenburg@gmail.com>
2023-12-17 20:51:40 +0100

    Added more options & min.js version

--- README.md
+++ README.md
@@ -1,24 +1,60 @@
 # SnowFlakesJs
-SnowFlakesJs is a script that can be inserted into an HTML header to add a snowfall effect to a website. You can customize the appearance of the snowflakes by adjusting the constants at the beginning of the script.
+**SnowFlakesJs** is a script that can be inserted into an HTML header to add a snowfall effect to a website. You can customize the appearance of the snowflakes by adjusting the constants at the beginning of the script.
 
-If you prefer not to use an external JavaScript file, you can simply copy the code and place it directly within script tags in your HTML file:<br>
-``<script>...</script>``
+To integrate the script into your HTML file, copy and paste the following line into the header:<br>
+``<script src="https://cdn.jsdelivr.net/gh/oskarmeyenburg/SnowFlakesJs/snowflakes.min.js"></script>``
+<br><br>
+If you prefer to modify the constants, add another script tag containing your adjustments. Refer to the list of available constants below for customization options.
+Here is an example for the usage with adjustments to SNOWFLAKE_MAX_SPEED and SNOWFLAKE_NUMBER.
+```
+<script src="https://cdn.jsdelivr.net/gh/oskarmeyenburg/SnowFlakesJs/snowflakes.min.js"></script>
+<script>
+    SNOWFLAKE_MAX_SPEED = 0.2;
+    SNOWFLAKE_NUMBER = 30;
+</script>
+```
 
 ## Customizable constants
+### SNOWFLAKE_START_DATE and SNOWFLAKE_END_DATE
+Set the time range during which the snowflake effect is active. The start date is inclusive and the end date is exclusive. By default the effect will be effect only in december.
+<br>Default:<br>``SNOWFLAKE_START_DATE = 1.12;``<br>``SNOWFLAKE_END_DATE = 1.1;``<br>
+
 ### SNOWFLAKE_MIN_SPEED and SNOWFLAKE_MAX_SPEED
-Determine the range of random downward velocities for each snowflake. The velocity is a percentage of the screen size, ranging from SNOWFLAKE_MIN_SPEED to SNOWFLAKE_MAX_SPEED.<br><br>
+Determine the range of random downward velocities for each snowflake. The velocity is a percentage of the screen size, ranging from SNOWFLAKE_MIN_SPEED to SNOWFLAKE_MAX_SPEED.
+<br>Default:<br>``SNOWFLAKE_MIN_SPEED = 0.05;``<br>``SNOWFLAKE_MAX_SPEED = 0.15;``<br>
 
 ### SNOWFLAKE_MIN_SIZE and SNOWFLAKE_MAX_SIZE
-Set the range of random sizes in pixels for each snowflake, varying from SNOWFLAKE_MIN_SIZE to SNOWFLAKE_MAX_SIZE.<br><br>
+Set the range of random sizes in pixels for each snowflake, varying from SNOWFLAKE_MIN_SIZE to SNOWFLAKE_MAX_SIZE.
+<br>Default:<br>``SNOWFLAKE_MIN_SIZE = 5;``<br>``SNOWFLAKE_MAX_SIZE = 15;``<br>
 
 ### SNOWFLAKE_ROTATION_SPEED
-Controls the rotation speed of snowflakes in degrees per second.<br><br>
+Control the rotation speed of snowflakes in degrees per second.
+<br>Default:<br>``SNOWFLAKE_ROTATION_SPEED = 25;``<br>
 
-### SNOWFLAKE_SPAWN_DELAY
-Determines the average delay between spawned snowflakes, adding a customizable rhythm to the falling snow.<br><br>
+### SNOWFLAKE_NUMBER
+Set the total count of snowflakes spawned at the beginning.
+<br>Default:<br>``SNOWFLAKE_NUMBER = 20;``<br>
+
+### SNOWFLAKE_CENTER_REDUCTION
+Reduce the spawning of snowflakes in the center of the screen.
+<br>Default:<br>``SNOWFLAKE_CENTER_REDUCTION = true;``<br>
 
 ### SNOWFLAKE_COLORS
-An array containing possible colors from which the color for each new snowflake is randomly chosen.<br><br>
+An array containing possible colors from which the color for each new snowflake is randomly chosen.
+<br>Default:<br>``SNOWFLAKE_COLORS = ["#dddddd", "#f2f2f2", "#ffffff"];``<br>
 
-### SNOWFLAKE_CIRCLE, SNOWFLAKE_CIRCLE_SYMBOL and SNOWFLAKE_SYMBOLS
-When the option SNOWFLAKE_CIRCLE is enabled, all snowflakes take on a simple circular shape. The symbol is defined by SNOWFLAKE_CIRCLE_SYMBOL. Otherwise each newly spawned snowflake is assigned a random symbol from SNOWFLAKE_SYMBOLS.
+### SNOWFLAKE_SYMBOLS
+Each newly spawned snowflake is assigned a random symbol from this array.
+<br>Default:<br>``SNOWFLAKE_SYMBOLS = ["❅", "❆", "*", "●"];``<br>
+
+### SNOWFLAKE_SHADOW
+Enable a slight shadow behind snowflakes.
+<br>Default:<br>``SNOWFLAKE_SHADOW = true;``<br>
+
+### SNOWFLAKE_SHADOW_COLOR
+Set the color of shadows behind snowflakes when SNOWFLAKE_SHADOW is enabled.
+<br>Default:<br>``SNOWFLAKE_SHADOW_COLOR = #000000;``<br>
+
+### SNOWFLAKE_SPAWN_ON_SCREEN
+Spawn snowflakes on the screen instead of above it when the site loads.
+<br>Default:<br>``SNOWFLAKE_SPAWN_ON_SCREEN = false;``<br>
--- snowflakes.js
+++ snowflakes.js
@@ -1,105 +1,132 @@
-const SNOWFLAKE_MIN_SPEED = 0.05;
-const SNOWFLAKE_MAX_SPEED = 0.15;
-const SNOWFLAKE_MIN_SIZE = 5;
-const SNOWFLAKE_MAX_SIZE = 15;
-const SNOWFLAKE_ROTATION_SPEED = 10;
-const SNOWFLAKE_SPAWN_DELAY = 0.3;
-const SNOWFLAKE_COLORS = ["#dddddd", "#eeeeee", "#ffffff"];
-const SNOWFLAKE_CIRCLE = true;
-const SNOWFLAKE_CIRCLE_SYMBOL = "●";
-const SNOWFLAKE_SYMBOLS = ["❅", "❆", "❃", "❇", "❊", "❋", "*"];
+// Create minimized version with https://minify-js.com
 
-var previous_time = 0;
-var next_flake_time = 0;
-var snowflakes = [];
+// Adjustable variables
+window.SNOWFLAKE_START_DATE = 1.12; // Included
+window.SNOWFLAKE_END_DATE = 1.1; // Excluded
+window.SNOWFLAKE_MIN_SPEED = 0.05;
+window.SNOWFLAKE_MAX_SPEED = 0.15;
+window.SNOWFLAKE_MIN_SIZE = 5;
+window.SNOWFLAKE_MAX_SIZE = 15;
+window.SNOWFLAKE_ROTATION_SPEED = 25; // Maximum rotation per second of snowflakes in degrees
+window.SNOWFLAKE_NUMBER = 20; // Total count of snowflakes
+window.SNOWFLAKE_CENTER_REDUCTION = true; // Less snowflakes in center of screen
+window.SNOWFLAKE_COLORS = ["#dddddd", "#f2f2f2", "#ffffff"]; // List of possible colors of snowflakes
+window.SNOWFLAKE_SYMBOLS = ["❅", "❆", "*", "●"]; // List of possible symbols of snowflakes
+window.SNOWFLAKE_SHADOW = true; // Show a shadow around snowflakes
+window.SNOWFLAKE_SHADOW_COLOR = "#000"; // Shadow color around snowflakes
+window.SNOWFLAKE_SPAWN_ON_SCREEN = false; // Spawn snowflakes on screen when loading
 
-var screen_width = 0;
-var screen_height = 0;
 
-var container = document.createElement("div");
-container.style.overflow = 'hidden';
-container.style.position = 'fixed';
-container.style.top = '0';
-container.style.left = '0';
-container.style.width = '100%';
-container.style.height = '100%';
-setTimeout(() => {document.body.appendChild(container)}, 1);
+// Constants
+const localDate = new Date();
+const current_date_number = localDate.getMonth() + 1 + localDate.getDate() * 0.01
+
 
 class SnowFlake {
-	constructor() {
-        let snowflake_speed = Math.random() * (SNOWFLAKE_MAX_SPEED - SNOWFLAKE_MIN_SPEED) + SNOWFLAKE_MIN_SPEED;
-        let snowflake_size = Math.random() * (SNOWFLAKE_MAX_SIZE - SNOWFLAKE_MIN_SIZE) + SNOWFLAKE_MIN_SIZE;
-        let snowflake_color = SNOWFLAKE_COLORS[Math.floor(Math.random() * SNOWFLAKE_COLORS.length)];
-        let snowflake_symbol = [SNOWFLAKE_SYMBOLS[Math.floor(Math.random() * SNOWFLAKE_SYMBOLS.length)], SNOWFLAKE_CIRCLE_SYMBOL][+SNOWFLAKE_CIRCLE];
+	constructor(y) {
+        let snowflake_speed = Math.random() * (window.SNOWFLAKE_MAX_SPEED - window.SNOWFLAKE_MIN_SPEED) + window.SNOWFLAKE_MIN_SPEED;
+        let snowflake_size = Math.random() * (window.SNOWFLAKE_MAX_SIZE - window.SNOWFLAKE_MIN_SIZE) + window.SNOWFLAKE_MIN_SIZE;
+        let snowflake_color = window.SNOWFLAKE_COLORS[Math.floor(Math.random() * window.SNOWFLAKE_COLORS.length)];
+        let snowflake_symbol = window.SNOWFLAKE_SYMBOLS[Math.floor(Math.random() * window.SNOWFLAKE_SYMBOLS.length)];
 
-        this.x = Math.random();
-        this.y = -0.1;
+        this.y = y;
+        this.reset_position();
         this.angle = Math.random() * 360;
-        this.vel_x = (Math.random() - 0.5) * snowflake_speed * 0.2;
         this.vel_y = snowflake_speed;
-        this.vel_angle = ((Math.random() - 0.5) * SNOWFLAKE_ROTATION_SPEED) ** 2;
+        this.vel_angle = (Math.random() * 2 - 1) * window.SNOWFLAKE_ROTATION_SPEED;
         this.wave_offset = Math.random() * 3000;
         this.div = document.createElement("div");
-        
-        this.div.style.position = 'fixed';
-
+        this.div.style.position = 'absolute';
         this.div.style.userSelect = 'none';
         this.div.style.webkitUserSelect = 'none';
         this.div.style.webkitTouchCallout = 'none';
         this.div.style.msUserSelect = 'none';
         this.div.unselectable = 'on';
-        this.div.style.overflow = "visible";
 
-        this.div.style.position = 'absolute';
-        this.div.innerHTML += "<p style='color:" + snowflake_color +  ";font-size:" + snowflake_size + "px;'>" + snowflake_symbol + "</p>";
+        let shadow_string = "";
+        if (window.SNOWFLAKE_SHADOW) {
+            shadow_string =  `text-shadow: 0 0 1px ${window.SNOWFLAKE_SHADOW_COLOR};`;
+        }
+        this.div.innerHTML += `<p style='color:${snowflake_color};font-size:${snowflake_size}px;${shadow_string}'>${snowflake_symbol}</p>`;
+        
         container.appendChild(this.div);
         snowflakes.push(this);
     }
   	update(delta_time, time) {
-        let wave_offset_x = Math.sin(time / 1000 + this.wave_offset) * 0.01;
-        let wave_offset_y = Math.cos(time / 1000 + this.wave_offset) * 0.01;
-        let wave_offset_angle = Math.cos(time / 1000 + this.wave_offset) * 0.01;
-
-        this.x += delta_time * this.vel_x;
     	this.y += delta_time * this.vel_y;
-        this.angle = this.angle + delta_time * this.vel_angle + wave_offset_angle;
+        this.angle += delta_time * this.vel_angle;
+        let rotation = `rotate(${this.angle}deg)`;
 
-        let x = (this.x + wave_offset_x) * screen_width;
-        let y = (this.y + wave_offset_y) * screen_height;
-
-        this.div.style.left = x + "px";
-        this.div.style.top = y + "px";
-        this.div.style.webkitTransform = "rotate(" + this.angle + "deg)";
-        this.div.style.mozTransform = "rotate(" + this.angle + "deg)";
-        this.div.style.msTransform = "rotate(" + this.angle + "deg)";
-        this.div.style.oTransform = "rotate(" + this.angle + "deg)";
-        this.div.style.transform = "rotate(" + this.angle + "deg)";
+        this.div.style.cssText = `
+            position: absolute;
+            left: ${(this.x + Math.sin(time / 1000 + this.wave_offset) * 0.01) * screen_width}px;
+            top: ${this.y * screen_height}px;
+            -webkit-transform: ${rotation};
+            -moz-transform: ${rotation};
+            -ms-transform: ${rotation};
+            -o-transform: ${rotation};
+            transform: ${rotation};
+        `;
 
         if (this.y > 1) {
-            this.delete();
+            this.reset_position();
         }
     }
-    delete() {
-        container.removeChild(this.div);
-        snowflakes.splice(snowflakes.indexOf(this), 1); 
+    reset_position() {
+        this.x = Math.random();
+        if (window.SNOWFLAKE_CENTER_REDUCTION) {
+            this.x = 1 / (1 + Math.exp(-10 * (this.x - 0.5)));
+        }
+        this.y -= 1.1
     }
 }
 
-(update = (time) => {
+const update = (time) => {
     var delta_time = (time - previous_time) * 0.001;
     previous_time = time;
 
     screen_width = container.clientWidth;
     screen_height = container.clientHeight;
 
-    if (time > next_flake_time) {
-        next_flake_time = time + 1000 * SNOWFLAKE_SPAWN_DELAY * (0.5 + Math.random());
-        new SnowFlake();
-    }
-
     for (i = snowflakes.length - 1; i >= 0; i--) {
         snowflakes[i].update(delta_time, time);
     }
     
     window.requestAnimationFrame(update);
-})(0)
+}
+
+// Globals
+var container;
+var previous_time = 0;
+var next_flake_time = 0;
+var snowflakes = [];
+var screen_width = 0;
+var screen_height = 0;
+
+const main = () => {
+    if (window.SNOWFLAKE_START_DATE > window.SNOWFLAKE_END_DATE) {
+        window.SNOWFLAKE_END_DATE += 12;
+    }
+    if (!(window.SNOWFLAKE_START_DATE < current_date_number < window.SNOWFLAKE_END_DATE)) {
+        return;
+    }
+
+    container = document.createElement("div");    
+    container.style.overflow = 'hidden';
+    container.style.position = 'fixed';
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100%';
+    container.style.height = '100%';
+    document.body.appendChild(container);
+
+    for (let y = 0; y < 1; y += 1 / window.SNOWFLAKE_NUMBER) {
+        new SnowFlake(y + 0.05 + (+window.SNOWFLAKE_SPAWN_ON_SCREEN));
+    }
+
+    window.SNOWFLAKE_SPAWN_ON_SCREEN = 0
+    window.requestAnimationFrame(update);
+}
+
+// Initial delayed setup
+window.requestAnimationFrame(main);
